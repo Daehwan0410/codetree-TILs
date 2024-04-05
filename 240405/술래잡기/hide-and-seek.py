91ad3219031124d09ea9116871a63be18e76dfea
Daehwan0410 <54808146+Daehwan0410@users.noreply.github.com>
2024-04-05 13:51:48 +0900

    Title: 술래잡기, Time: 152ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240405/술래잡기/hide-and-seek.py
+++ 240405/술래잡기/hide-and-seek.py
@@ -17,12 +17,12 @@ a = [i for i in range(2, n)]
 b = [i for i in range(1, n)]
 a = a + b
 a.append(n-1)
-a.sort(reverse = True)
+a.sort()
 a = deque(a)
 count = 0
 check = 1
 answer = 0
-# print(hlist)
+checkReverse = False
 for _ in range(1,k+1):
     #move hide people
     for i , (x, y , d, bt) in enumerate(hlist):
@@ -34,27 +34,33 @@ for _ in range(1,k+1):
             if 0<= nx <n and 0 <= ny < n and [nx, ny] != [px , py]: 
                 hlist[i][:2] = [nx, ny]
             else:
-                if i ==2:
-                    print(nx, ny, d, px, py)
                 d = (d+2) % 4
                 nx = x + dxy[d][0]
                 ny = y + dxy[d][1]
                 if nx != px and ny != py:
                     hlist[i]= [nx, ny, d, bt]
     #move people               
-    if [px, py] ==[0, 0] :
-        a.reverse()
-    elif [px, py] == [n//2, n//2]:
-        a =list(a)
-        a = deque(sorted(a))
+
+
     px = px + dxy[pd][0]
     py = py + dxy[pd][1]
     count +=1
-#     print('count:', count,'check', check)
     if count == check:
-        a.append(count)
-        check = a.popleft()
-        pd = (pd+1) % 4
+        if [px, py] ==[0, 0] :
+            a.reverse()
+            checkReverse = True
+            pd = 2
+        elif [px, py] == [n//2, n//2]:
+            a =list(a)
+            a = deque(sorted(a))
+            checkReverse= False
+        else:
+            a.append(count)
+            check = a.popleft()
+            if checkReverse:
+                pd = (pd-1) % 4
+            else:
+                pd = (pd+1) % 4
         count = 0
     tSum = 0
     for i in range(3):
